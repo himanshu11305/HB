9936280ae78c09bc3a82f0d7501d625d5e8397c2
Himanshu Bhardwaj <Himanshu11305@yahoo.com>
2020-05-03 19:05:10 -0400

    commit mySecondRproject

---
+++ R/aSimpleFile.R
@@ -0,0 +1,14 @@
+#' Add together two numbers.
+#'
+#' This is truly a great and much-needed function
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @export
+#' @examples
+#' aGreatFunction(1, 1)
+#' aGreatFunction(10, 1)
+aSimpleFunction <- function(x,y){
+  return(x+y)
+}
